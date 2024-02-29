6f98431cce486c7ff8ff2bd61a0496f24a47ea8b
Quinn Pollock <contact@quinnpollock.net>
2024-02-29 08:38:13 -0500

    Update dateUtils.ts

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,7 +1,7 @@
 export function toDate(dateString) {
 	const dates = dateString.split('-')
 	const year = Number(dates[0])
-	const month = Number(dates[1]) - 1
-	const date = new Date(year, month, 1)
+	const month = Number(dates[1])
+	const date = new Date(Date.UTC(year, month, 1))
 	return date
 }
